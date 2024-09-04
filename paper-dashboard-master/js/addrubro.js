82024f8dc3dab592258454fe92be8d22e09c5b3e
Mateomelano <mateomelano2003@gmail.com>
2024-09-04 08:11:59 -0300

    Comits recientes

--- paper-dashboard-master/js/addrubro.js
+++ paper-dashboard-master/js/addrubro.js
@@ -1,19 +1,20 @@
+(function() {
+        const items = document.querySelectorAll('.item');
+        console.log(items); // Verifica que se están seleccionando los elementos correctamente
 
-    const items = document.querySelectorAll('.item');
-    console.log(items); // Verifica que se están seleccionando los elementos correctamente
+        items.forEach(item => {
+            const productName = item.querySelector('h2').textContent.trim();
+            console.log(productName); // Verifica el nombre del producto
 
-    items.forEach(item => {
-        const productName = item.querySelector('h2').textContent.trim();
-        console.log(productName); // Verifica el nombre del producto
+            if (productName === 'Zapatos Nike') {
+                const newParagraph = document.createElement('p');
+                newParagraph.className = 'rubro';
+                newParagraph.innerHTML = 'Rubro:Calzado';
 
-        if (productName === 'Zapatos Nike') {
-            const newParagraph = document.createElement('p');
-            newParagraph.className = 'rubro';
-            newParagraph.innerHTML = 'Rubro:Calzado';
+                const marcaElement = item.querySelector('.marca');
+                console.log(marcaElement); // Verifica que se encuentra el elemento con la clase 'marca'
 
-            const marcaElement = item.querySelector('.marca');
-            console.log(marcaElement); // Verifica que se encuentra el elemento con la clase 'marca'
-
-            marcaElement.insertAdjacentElement('afterend', newParagraph);
-        }
-    });
+                marcaElement.insertAdjacentElement('afterend', newParagraph);
+            }
+        });
+})();
